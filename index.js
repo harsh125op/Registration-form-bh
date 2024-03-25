3a7d03d6de2ba74b428746074c08312331436c7c
harsh125op <72938822+harsh125op@users.noreply.github.com>
2024-03-25 16:59:43 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,69 @@
+const express = require("express")
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const dotenv = require("dotenv");
+
+const app = express();
+dotenv.config();
+
+const port = process.env.PORT || 5000;
+const username = "mahantiharsh";
+const password = "GYlrPYKFqVHxetJa";
+
+
+mongoose.connect(`mongodb+srv://${username}:${password}@cluster0.h2pyg1f.mongodb.net/registrationFormDB`,{
+    
+})
+
+const registerationSchema = new mongoose.Schema({
+    name : String,
+    email : String,
+    password : String
+});
+
+const registration = mongoose.model("Registration", registerationSchema);
+
+app.use(bodyParser.urlencoded ({ extended: true }));
+app.use(bodyParser.json());
+
+
+app.get("/",(req,res) => {
+    res.sendFile(__dirname + "/pages/index.html")
+})
+
+app.post("/register", async (req, res) =>{
+    try{
+            const {name, email, password} = req.body;
+            const existingUser = await registration.findOne({email : email });
+            if(!existingUser) {
+                    const registrationData = new registration({
+                        name, 
+                        email,
+                        password,
+                    });
+                    await registrationData.save();
+                    res.redirect("/success");
+                        }
+            else{
+            console.log("User alreadyexist");
+            res.redirect("/error");
+    }}
+    catch (error) {
+        console.log(error);
+        res.redirect("/error");
+    }
+})
+
+app.get("/success", (req, res)=>{
+    res.sendFile (__dirname+"/pages/sucess.html");
+})
+app.get("/error", (req, res)=>{
+    res.sendFile (__dirname+"/pages/error.html");
+})
+
+app.listen(port, ()=>{
+    console.log(`server running on port ${port}`);
+}
+)
+
+
